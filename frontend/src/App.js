05099189c4f5b19c4cf93d3941e9b88b6691bfe0
ylongly7 <orzyaaaah@163.com>
2020-04-12 10:36:28 +0800

    添加前端请求后端api的示例代码，方便其他同学继续工作

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,69 @@
 import React from 'react';
 import logo from './logo.svg';
+import $ from 'jquery'
 import './App.css';
 
 function App() {
+	//演示api
+	if (true){
+	//全部todolist
+	   $.ajax(
+		  {
+			"type": 'GET',
+			"url":"api/tasks",
+			"success":(resbody)=>{
+			  console.log(resbody)
+			},
+			"error":(err)=>{
+				console.log(err,err.status,err.responseText)
+			}
+		  }
+	  ); 
+	 //添加todoitem 1586658034.2166107
+	 /*  $.ajax(
+		  {
+			"type": 'POST',
+			"url":"api/tasks",
+			"data":JSON.stringify({"content":"newItem"}),
+			"success":(resbody)=>{
+			  console.log(resbody)
+			},
+			"error":(err)=>{
+				console.log(err,err.status,err.responseText)
+			}
+		  }
+	  ); */
+	  
+	  //删除todoitem url格式：api/tasks/id
+	  $.ajax(
+		  {
+			"type": 'DELETE',
+			"url":"api/tasks/"+"1586658034.2166107",			 
+			"success":(resbody)=>{
+			  console.log(resbody)
+			},
+			"error":(err)=>{
+				console.log(err,err.status,err.responseText)
+			}
+		  }
+	  );
+	  
+	  //修改todoitem  
+	  $.ajax(
+		  {
+			"type": 'PUT',
+			"url":"api/tasks/" ,
+			"data":JSON.stringify({"content":"231232", "id":"1586486287.794215"}),
+			"success":(resbody)=>{
+			  console.log(resbody)
+			},
+			"error":(err)=>{
+				console.log(err,err.status,err.responseText)
+			}
+		  }
+	  );
+	  
+	}
   return (
     <div className="App">
       <header className="App-header">
